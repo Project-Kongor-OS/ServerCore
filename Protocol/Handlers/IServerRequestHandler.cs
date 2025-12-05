2d90a9a3de2d753927e3f1d72ded6aa74c6663df
Mark Klara <mrhappyasthma@gmail.com>
2025-12-05 15:23:53 -0800

    Migrate request handler API to ServerCore.

---
+++ Protocol/Handlers/IServerRequestHandler.cs
@@ -0,0 +1,8 @@
+namespace ZORGATH;
+
+/// <summary>
+///    Interface for a `server_requester.php` function request handlers.
+/// </summary>
+public interface IServerRequestHandler : IRequestHandler
+{
+}
