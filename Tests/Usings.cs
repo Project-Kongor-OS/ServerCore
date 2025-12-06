f2a71cb2a8c181484c70d2e9a432c64c71f01e81
Mark Klara <mrhappyasthma@gmail.com>
2025-12-05 19:34:29 -0800

    Restructure project to allow test to run properly.

---
+++ Tests/Usings.cs
@@ -0,0 +1,29 @@
+﻿#region Microsoft Using Directives
+global using Microsoft.AspNetCore.Mvc;
+#endregion
+
+#region System using Directives
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+#endregion
+
+#region Test Directives
+global using Moq;
+global using Xunit;
+#endregion
+
+#region PhpSerializerNET Using Directives
+global using PhpSerializerNET;
+#endregion
+
+#region ProjectKongor Protocols
+global using ProjectKongor.Protocol.HTTP.Responses;
+global using ProjectKongor.Protocol.Handlers;
+global using ProjectKongor.Protocol.Registries;
+global using ProjectKongor.Protocol.Services;
+#endregion
+
+#region ProjectKongor Shared
+global using KONGOR.Shared.Handlers.Client;
+#endregion
